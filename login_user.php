b1f4ef0d5e361e3766e264b2b618cc2e5056f44a
Edward Seo <edwardhwseo@gmail.com>
2023-03-22 10:36:49 -0500

    Added user_id session variable

--- login_user.php
+++ login_user.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
 
-    if(isset($_GET['username']) && isset($_GET['user_role_id'])){
-        $_SESSION['username'] = $_GET['username'];
+    if(isset($_GET['user_id']) && isset($_GET['username']) && isset($_GET['user_role_id'])){
+        $_SESSION['user_id'] = $_GET['user_id'];
         $_SESSION['user_role_id'] = $_GET['user_role_id'];
+        $_SESSION['username'] = $_GET['username'];
     }
 ?>
